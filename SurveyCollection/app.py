21dd27304c1430283ab404cc3d8d679e80d7a396
Kobbyjust <sagarinokoe1@gmail.com>
2023-01-02 10:13:22 +0000

    S3Cloudfront for web hosting

--- SurveyCollection/app.py
+++ SurveyCollection/app.py
@@ -2,10 +2,36 @@
 
 import aws_cdk as cdk
 
+
 from survey_collection.survey_collection_stack import SurveyCollectionStack
+from s3CloudfrontHostingSite.site_stack import StaticSiteStack
 
 
 app = cdk.App()
-SurveyCollectionStack(app, "survey-collection")
+#SurveyCollectionStack(app, "survey-collection")
+
+props = {
+    "namespace": app.node.try_get_context("namespace"),
+    "domain_name": app.node.try_get_context("domain_name"),
+    "sub_domain_name": app.node.try_get_context("sub_domain_name"),
+    "domain_certificate_arn": app.node.try_get_context(
+        "domain_certificate_arn"
+    ),
+    "enable_s3_website_endpoint": app.node.try_get_context(
+        "enable_s3_website_endpoint"
+    ),
+    "origin_custom_header_parameter_name": app.node.try_get_context(
+        "origin_custom_header_parameter_name"
+    ),
+    "hosted_zone_id": app.node.try_get_context("hosted_zone_id"),
+    "hosted_zone_name": app.node.try_get_context("hosted_zone_name"),
+}
+
+StaticSite = StaticSiteStack(
+    scope=app,
+    construct_id=f"{props['namespace']}-stack",
+    props=props,
+    description="Static Site using S3, CloudFront and Route53",
+)
 
 app.synth()
---
+++ SurveyCollection/s3CloudfrontHostingSite/site_stack.py
@@ -0,0 +1,31 @@
+from aws_cdk import CfnOutput, Stack
+from s3CloudfrontHostingSite.frontend import StaticSitePublicS3
+
+
+class StaticSiteStack(Stack):
+    def __init__(self, scope, construct_id, props, **kwargs):
+        super().__init__(scope, construct_id, **kwargs)
+
+        site = StaticSitePublicS3(
+                self,
+                f"{props['namespace']}-construct",
+
+                origin_referer_header_parameter_name=props[
+                    "origin_custom_header_parameter_name"
+                ],
+               
+            )
+
+        # Add stack outputs
+        CfnOutput(
+            self,
+            "SiteBucketName",
+            value=site.bucket.bucket_name,
+        )
+        CfnOutput(
+            self,
+            "DistributionId",
+            value=site.distribution.distribution_id,
+        )
+
+#
